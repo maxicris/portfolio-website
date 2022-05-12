5b595e59573b17912f7342bed83e2460a43c782c
Maxi Cris <maxicris@protonmail.com>
2022-05-12 15:57:04 -0300

    transiciones de paginas y navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,18 @@
-// Animacion de menu mobile
-
-function myFunction() {
-  var x = document.getElementById("nav-mobile");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
-}
-$(".menu-toggle").click(function () {
-  $(".nav").toggleClass("mobile-nav");
-  $(this).toggleClass("is-active");
-});
+var lastScrollTop = 0;
+window.addEventListener(
+  "scroll",
+  function () {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+      document.getElementById("nav").classList.remove("border-bottom-nav");
+      document.getElementById("nav").classList.add("display-none-nav");
+      document.getElementById("nav").classList.remove("display-flex-nav");
+    } else {
+      document.getElementById("nav").classList.add("border-bottom-nav");
+      document.getElementById("nav").classList.remove("display-none-nav");
+      document.getElementById("nav").classList.add("display-flex-nav");
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  },
+  false
+);
