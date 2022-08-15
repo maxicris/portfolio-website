5bedc35c837241b0a77a92c1f534db99c684ad56
Maxi Cris <maxicris@protonmail.com>
2022-08-15 11:34:52 -0300

    Agregado de gitignore

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,18 @@ window.addEventListener(
   function () {
     let st = window.pageYOffset || document.documentElement.scrollTop;
     if (st > lastScrollTop) {
-      document.getElementById("nav").classList.remove("border-bottom-nav");
       document.getElementById("nav").classList.add("display-none-nav");
       document.getElementById("nav").classList.remove("display-flex-nav");
     } else {
-      document.getElementById("nav").classList.add("border-bottom-nav");
       document.getElementById("nav").classList.remove("display-none-nav");
       document.getElementById("nav").classList.add("display-flex-nav");
     }
     lastScrollTop = st <= 0 ? 0 : st;
+    if (lastScrollTop <= 120) {
+      document.getElementById("nav").classList.add("background-nav-none");
+    } else {
+      document.getElementById("nav").classList.remove("background-nav-none");
+    }
   },
   false
 );
